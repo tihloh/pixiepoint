06bf429ef304cdc1f9c66f21ba77cad6f4218b93
Christian Bustamante <tihloh12@gmail.com>
2026-09-01 01:21:02 +0800

    Add Prefab permission subject model

---
+++ app/Models/PermissionUser.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PixiePoint\App\Models;
+
+use Tihloh\Prefab\Permissions\Contracts\PermissionSubjectInterface;
+
+final class PermissionUser implements PermissionSubjectInterface
+{
+    /** @param array<int|string> $groupIds */
+    public function __construct(
+        private int|string $id,
+        private array $groupIds = [],
+    ) {}
+
+    public function permissionSubjectId(): int|string
+    {
+        return $this->id;
+    }
+
+    public function permissionGroupIds(): array
+    {
+        return $this->groupIds;
+    }
+}
