affddd0ea87bc9ea8820d6bae63af71ba14966bd
Christian Bustamante <tihloh12@gmail.com>
2026-09-01 01:06:25 +0800

    Add router model

---
+++ app/Models/Router.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PixiePoint\App\Models;
+
+use PDO;
+
+final class Router
+{
+    public function __construct(private PDO $db) {}
+
+    public function enabledByIdentity(string $identity): array|false
+    {
+        $stmt = $this->db->prepare('SELECT * FROM routers WHERE identity=? AND enabled=1');
+        $stmt->execute([$identity]);
+        return $stmt->fetch();
+    }
+}
