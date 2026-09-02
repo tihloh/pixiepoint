2e1c40a09fcced3285ae17ca1a3f92791baabd9d
Christian Bustamante <tihloh12@gmail.com>
2026-09-02 21:15:07 +0800

    Register router installer endpoint

--- app/Admin/Routers/routes.php
+++ app/Admin/Routers/routes.php
@@ -7,4 +7,5 @@ use Tihloh\Prefab\Routes\RouteManager;
 return static function (RouteManager $routes, array $c): void {
     $routes->get('/admin/routers', [$c['admin.routers'], 'index'])->name('admin.routers.index')->auth()->permission('routers.view')->middleware('prefab.access');
     $routes->post('/admin/routers', [$c['admin.routers'], 'index'])->name('admin.routers.store')->auth()->permission('routers.manage')->middleware('prefab.access');
+    $routes->get('/api/router/install', [$c['router.agent'], 'install'])->name('api.router.install');
 };
