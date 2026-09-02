d09ae90a7df204be8e0ff38a0517fb02f6a9dd61
Christian Bustamante <tihloh12@gmail.com>
2026-09-02 20:54:54 +0800

    Add router poll and acknowledgement routes

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -6,6 +6,8 @@ use Tihloh\Prefab\Routes\RouteManager;
 
 return static function (RouteManager $routes, array $c): void {
     $routes->get('/hotspot/health', [$c['api'], 'health'])->name('api.health');
+    $routes->get('/api/router/poll', [$c['router.agent'], 'poll'])->name('api.router.poll');
+    $routes->get('/api/router/ack', [$c['router.agent'], 'ack'])->name('api.router.ack');
     $routes->post('/api/accounting', [$c['api'], 'accounting'])->name('api.accounting');
     $routes->post('/api/router/login-event', [$c['api'], 'loginEvent'])->name('api.router.login-event');
 };
