07fad0954eb2fba76641775c2d690f63e094be60
Christian Bustamante <tihloh12@gmail.com>
2026-09-02 22:02:31 +0800

    Add protected router test route

--- app/Admin/Routers/routes.php
+++ app/Admin/Routers/routes.php
@@ -7,6 +7,7 @@ use Tihloh\Prefab\Routes\RouteManager;
 return static function (RouteManager $routes, array $c): void {
     $routes->get('/admin/routers', [$c['admin.routers'], 'index'])->name('admin.routers.index')->auth()->permission('routers.view')->middleware('prefab.access');
     $routes->post('/admin/routers', [$c['admin.routers'], 'index'])->name('admin.routers.store')->auth()->permission('routers.manage')->middleware('prefab.access');
+    $routes->post('/admin/routers/{id}/test', [$c['router.agent'], 'test'])->name('admin.routers.test')->auth()->permission('routers.manage')->middleware('prefab.access');
     $routes->get('/api/router/install/{token}', [$c['router.agent'], 'install'])->name('api.router.install');
     $routes->get('/api/router/poll/{token}', [$c['router.agent'], 'poll'])->name('api.router.poll.path');
     $routes->get('/api/router/ack/{token}/{id}/{status}', [$c['router.agent'], 'ack'])->name('api.router.ack.path');
