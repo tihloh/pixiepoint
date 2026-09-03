944de29f6ec4a841749249c7ade7d5dd94f381c1
Christian Bustamante <tihloh12@gmail.com>
2026-09-03 20:49:17 +0800

    Add permissions index route

--- app/Admin/Permissions/routes.php
+++ app/Admin/Permissions/routes.php
@@ -6,6 +6,13 @@ use Tihloh\Prefab\Routes\RouteManager;
 
 return static function (RouteManager $routes, array $c): void {
     $routes
+        ->get('/admin/permissions', [$c['admin.permissions'], 'users'])
+        ->name('admin.permissions.index')
+        ->auth()
+        ->permission('permissions.manage')
+        ->middleware('prefab.access');
+
+    $routes
         ->matchMethods(['GET', 'POST'], '/admin/permissions/{id}', [$c['admin.permissions'], 'index'])
         ->name('admin.permissions.user')
         ->auth()
