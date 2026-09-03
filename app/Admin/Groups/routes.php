3ecae7f7d3b34463050dafaed07eb4e051440ae3
Christian Bustamante <tihloh12@gmail.com>
2026-09-03 22:50:26 +0800

    Use dedicated group list and edit routes

--- app/Admin/Groups/routes.php
+++ app/Admin/Groups/routes.php
@@ -5,6 +5,17 @@ declare(strict_types=1);
 use Tihloh\Prefab\Routes\RouteManager;
 
 return static function (RouteManager $routes, array $c): void {
-    $routes->matchMethods(['GET','POST'], '/admin/groups', [$c['admin.groups'], 'index'])->name('admin.groups.index')->auth()->permission('groups.manage')->middleware('prefab.access');
-    $routes->matchMethods(['GET','POST'], '/admin/groups/{id}', [$c['admin.groups'], 'index'])->name('admin.groups.manage')->auth()->permission('groups.manage')->middleware('prefab.access');
+    $routes
+        ->matchMethods(['GET', 'POST'], '/admin/groups', [$c['admin.groups'], 'index'])
+        ->name('admin.groups.index')
+        ->auth()
+        ->permission('groups.manage')
+        ->middleware('prefab.access');
+
+    $routes
+        ->matchMethods(['GET', 'POST'], '/admin/groups/{id}/edit', [$c['admin.groups'], 'edit'])
+        ->name('admin.groups.edit')
+        ->auth()
+        ->permission('groups.manage')
+        ->middleware('prefab.access');
 };
