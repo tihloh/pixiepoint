3911d2a451214b622e33f9a5fc8782601ed2de97
Christian Bustamante <tihloh12@gmail.com>
2026-09-03 22:16:07 +0800

    Allow authorized account creation from Users

--- app/Admin/Users/routes.php
+++ app/Admin/Users/routes.php
@@ -6,7 +6,7 @@ use Tihloh\Prefab\Routes\RouteManager;
 
 return static function (RouteManager $routes, array $c): void {
     $routes
-        ->get('/admin/users', [$c['admin.users'], 'index'])
+        ->matchMethods(['GET', 'POST'], '/admin/users', [$c['admin.users'], 'index'])
         ->name('admin.users.index')
         ->auth()
         ->permission('users.view')
