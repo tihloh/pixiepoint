e17d4403a2c93c6456fc67fee5646fd967507747
Christian Bustamante <tihloh12@gmail.com>
2026-09-03 22:23:25 +0800

    Add self-service profile route

--- app/Admin/Permissions/routes.php
+++ app/Admin/Permissions/routes.php
@@ -6,9 +6,14 @@ use Tihloh\Prefab\Routes\RouteManager;
 
 return static function (RouteManager $routes, array $c): void {
     $routes
+        ->matchMethods(['GET', 'POST'], '/profile', [$c['admin.permissions'], 'profile'])
+        ->name('profile')
+        ->auth()
+        ->middleware('prefab.access');
+
+    $routes
         ->matchMethods(['GET', 'POST'], '/admin/users/{id}', [$c['admin.permissions'], 'index'])
         ->name('admin.users.manage')
         ->auth()
-        ->permission('users.view')
         ->middleware('prefab.access');
 };
