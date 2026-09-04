4e1db99aa92733e4902060894dd946027fe84984
Christian Bustamante <tihloh12@gmail.com>
2026-09-05 03:01:55 +0800

    Remove duplicate import of FileManager in PrefabKernel

--- app/Services/PrefabKernel.php
+++ app/Services/PrefabKernel.php
@@ -8,7 +8,6 @@ use PDO;
 use PixiePoint\AppUserFactory;
 use Tihloh\Prefab\Auth\Services\AuthManager;
 use Tihloh\Prefab\Files\FileManager;
-use Tihloh\Prefab\Files\FileManager;
 use Tihloh\Prefab\Logs\Services\LogManager;
 use Tihloh\Prefab\Permissions\Services\PermissionManager;
 use Tihloh\Prefab\PrefabConfig;
