d5ee113a103b381c7b6415dd021fb1e7b3cb89f6
Christian Bustamante <tihloh12@gmail.com>
2026-09-06 01:20:14 +0800

    Remove CDN SRI hashes from Bootstrap and jQuery

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -29,11 +29,11 @@ $sidebarReturn = preg_match('#^/(?:admin|dashboard)(?:/|$)#', $path) ? $path : '
     <meta name="theme-color" content="#07111f">
     <title><?= e($title) ?> · <?= $name ?></title>
     <style>html,body{min-height:100%;background:#07111f}body{margin:0;background:radial-gradient(circle at 10% 0,#302265 0,transparent 31rem),#07111f}</style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/app.css?v=<?= e($cssVersion) ?>">
     <link rel="stylesheet" href="/assets/admin.css?v=<?= e($cssVersion) ?>">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwxH9JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <?php if ($dashboard): ?>
