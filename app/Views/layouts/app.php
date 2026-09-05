6bf577049811a239bda305b9687f2f54b6faea0d
Christian Bustamante <tihloh12@gmail.com>
2026-09-06 01:17:29 +0800

    Fix Bootstrap 5.3.8 SRI integrity hash

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -33,7 +33,7 @@ $sidebarReturn = preg_match('#^/(?:admin|dashboard)(?:/|$)#', $path) ? $path : '
     <link rel="stylesheet" href="/assets/app.css?v=<?= e($cssVersion) ?>">
     <link rel="stylesheet" href="/assets/admin.css?v=<?= e($cssVersion) ?>">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwxH9JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
 <?php if ($dashboard): ?>
