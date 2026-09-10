e61614fb813b8146db419bfe963e269a61fb4b5c
Christian Bustamante <tihloh12@gmail.com>
2026-09-10 22:00:56 +0800

    Preserve local origin for JuanFi requests

--- public/assets/hotspot-embed.js
+++ public/assets/hotspot-embed.js
@@ -5,6 +5,14 @@
   var url = origin + '/hotspot/compat' + (location.search || '');
   url += (url.indexOf('?') >= 0 ? '&' : '?') + '_embed=' + Date.now();
 
+  function hostedAssets(html) {
+    return html
+      .replace(/\b(src|href)=(['"])\/(?!\/)/gi, '$1=$2' + origin + '/')
+      .replace(/\b(src|href)=([^'"\s>]+)\/(?!\/)/gi, function (match, attr, value) {
+        return match;
+      });
+  }
+
   fetch(url, {
     method: 'GET',
     mode: 'cors',
@@ -14,9 +22,8 @@
     if (!response.ok) throw new Error('HTTP ' + response.status);
     return response.text();
   }).then(function (html) {
-    html = html.replace(/<head([^>]*)>/i, '<head$1><base href="' + origin + '/">');
     document.open();
-    document.write(html);
+    document.write(hostedAssets(html));
     document.close();
   }).catch(function (error) {
     document.body.innerHTML = '<main style="font-family:sans-serif;padding:24px;max-width:560px;margin:auto"><h1>PixiePoint unavailable</h1><p>The hosted portal could not be loaded.</p><p><small>' + String(error.message || error).replace(/[&<>"']/g, function (c) { return ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'})[c]; }) + '</small></p></main>';
